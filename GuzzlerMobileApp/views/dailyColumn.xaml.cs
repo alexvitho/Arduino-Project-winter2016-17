fea860441c6b988758a8cbc333572321c87bea7e
alexvitho <alexvitho@gmail.com>
2017-01-24 01:07:43 +0200

    column graph added

--- GuzzlerMobileApp/views/dailyColumn.xaml.cs
+++ GuzzlerMobileApp/views/dailyColumn.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using LiveCharts;
+using LiveCharts.Uwp;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,9 +28,10 @@ namespace GuzzlerMobileApp.views
     {
 
         public string DevName { get; private set; }
-       public List<powerDayItem> powerPerHour { get; private set; }
+        public List<powerDayItem> powerPerHour { get; private set; }
         public string DevGuzzeled { get; private set; }
         DateTime Date { get; set; }
+        deviceGraphAnalysis analysis = new deviceGraphAnalysis();
 
         public dailyColumn(DateTime DateTime, string name = null)
         {
@@ -40,9 +43,39 @@ namespace GuzzlerMobileApp.views
             else
                 Date = DateTime;
             this.InitializeComponent();
+            double[] DailyArray =analysis.getDailyPowerPerHour(Date.ToUniversalTime(), DevName);
+            double tarrif = analysis.getElectricityTaarif("Israel", "2017");
+            for (int i = 0; i < 24; i++)
+            {
+                DailyArray[i] *= tarrif;
+                DailyArray[i] = Math.Round(DailyArray[i],3);
+            }
+            SeriesCollection = new SeriesCollection();
+           
+                SeriesCollection.Add(new ColumnSeries()
 
+                {
+                    Title = "Nis",
+                    Values = new ChartValues<double>(DailyArray),
+                //    LabelPoint= new Func<ChartPoint, string>(p => p.Y.ToString()+" NIS")
+                });
+
+           
+
+
+            Labels = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24" };
+            Formatter = value => value.ToString("N");
+            valFormatter = new Func<double, string>(p => p.ToString() + " NIS");
+            DataContext = this;
 
         }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+        public Func<double, string> valFormatter { get; set; }
+        public double[] Values { get; set; }
+
 
 
         private void backButton_Click(object sender, RoutedEventArgs e)
@@ -51,4 +84,5 @@ namespace GuzzlerMobileApp.views
             Window.Current.Activate();
         }
     }
+    
 }
